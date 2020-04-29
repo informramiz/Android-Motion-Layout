a3ecaf256872a0104289f34111371b97e72b4370
Ramiz Raja <informramiz@gmail.com>
2020-04-30 02:46:35 +0500

    feat: Complete collapsing toolbar anim

--- MotionLayoutAnimations/app/src/main/java/github/informramiz/motionlayoutanimations/Step8Activity.kt
+++ MotionLayoutAnimations/app/src/main/java/github/informramiz/motionlayoutanimations/Step8Activity.kt
@@ -2,6 +2,8 @@ package github.informramiz.motionlayoutanimations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
 
 class Step8Activity : AppCompatActivity() {
 
@@ -13,7 +15,17 @@ class Step8Activity : AppCompatActivity() {
     }
 
     private fun coordinateMotion() {
-        // TODO: set progress of MotionLayout based on an AppBarLayout.OnOffsetChangedListener
+        val appBar = findViewById<AppBarLayout>(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+        appBar.addOnOffsetChangeProgressListener { progress ->
+            motionLayout.progress = progress
+        }
+    }
 
+    private fun AppBarLayout.addOnOffsetChangeProgressListener(callback: (progress: Float) -> Unit) {
+        addOnOffsetChangedListener( AppBarLayout.OnOffsetChangedListener{ _, verticalOffset ->
+            val seekPosition = -verticalOffset / totalScrollRange.toFloat()
+            callback(seekPosition)
+        })
     }
 }
